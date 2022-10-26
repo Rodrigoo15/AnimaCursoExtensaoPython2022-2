76f0379687ca24cbd7b4e6f0723f3db87a6d8e83
Rodrigo015 <116282297+Rodrigoo15@users.noreply.github.com>
2022-10-26 23:32:49 +0000

    aula 26-10

--- main.py
+++ main.py
@@ -12,3 +12,18 @@ print(nome)
 
 # Variável "minh idade é" completando o com o conteudo da variável.
 print("minha idade é" + idade)
+
+
+# aula 26/10
+
+# para digitar qualquer coisa input() codigo de entrada
+nome = input("Digite seu nome ")
+idade = int(input("Sua idade é "))
+
+# Escreva seu nome;
+print("Digite seu nome: ")
+
+# Estrutura condicional - "SE" (if)
+# Se a pessoa for maior de idade , mostre "Você é maior de idade. Otimo! já pode beber"
+if (idade >= 18):
+  print("Você é maior de idade, ótimo! Já pode beber")
