e9b529014e5de23e5ae69e417b7624ea94873546
Rodrigo015 <116282297+Rodrigoo15@users.noreply.github.com>
2022-10-26 20:55:59 +0000

    String nome e idade.

--- main.py
+++ main.py
@@ -2,3 +2,10 @@
 # print () = comando de saida
 
 print ("Hello world!")
+
+# Quando quiser guardar uma String! (frase)
+nome = "Rodrigo Oliveira"
+idade = 21
+
+# exibir o nome na variável nome
+print(nome)
