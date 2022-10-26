656ecd3e01aece9f66f5f7decc9dab9ce74ddf26
Rodrigo015 <116282297+Rodrigoo15@users.noreply.github.com>
2022-10-26 21:03:03 +0000

    idade

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ print ("Hello world!")
 
 # Quando quiser guardar uma String! (frase)
 nome = "Rodrigo Oliveira"
-idade = 21
+idade = "21"
 
 # exibir o nome na variável nome
 print(nome)
-print(idade)
+
+# Variável "minh idade é" completando o com o conteudo da variável.
+print("minha idade é" + idade)
