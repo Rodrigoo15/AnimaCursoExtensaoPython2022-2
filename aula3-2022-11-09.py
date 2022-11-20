2efa7ff0a9fb5c442d02a6d6eafc5dfdb1c290b9
Rodrigo015 <116282297+Rodrigoo15@users.noreply.github.com>
2022-11-20 20:28:32 +0000

    Funções

--- aula3-2022-11-09.py
+++ aula3-2022-11-09.py
@@ -19,12 +19,12 @@ print(frutas)
 print(frutas[2])
 
 # Exibir quantas frutas tem na minha lista ?
-print (len(frutas)) # len = length
+print(len(frutas))  # len = length
 
 #para incluir uma fruta
 frutas.append("manga")
-print (len(frutas)) # len = length
-print (frutas)
+print(len(frutas))  # len = length
+print(frutas)
 
 print(frutas[0])
 print(frutas[1])
@@ -32,15 +32,14 @@ print(frutas[2])
 print(frutas[3])
 #print(frutas[4])
 
-print ("exemplo das frutas com while...")
+print("exemplo das frutas com while...")
 frutas.append("uva")
 
-i=0
-while(i<len(frutas)):
-  print (frutas[i])
-  i=i+1
+i = 0
+while (i < len(frutas)):
+  print(frutas[i])
+  i = i + 1
 
-print ("Exemplo daas frutas com FOR")
+print("Exemplo daas frutas com FOR")
 for fruta in frutas:
-  print (fruta)
-
+  print(fruta)
